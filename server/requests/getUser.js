9d15fd2c4b9be851629ed3f86e53423357a1fc41
hzietek <hubazietek3@gmail.com>
2019-11-11 13:17:37 +0100

    sql fix

--- server/requests/getUser.js
+++ server/requests/getUser.js
@@ -1,7 +1,7 @@
 const db = require ('../db');
 
 const getUser = (req, res, next) => {
-    const sql = 'SELECT * FROM profiles';
+    const sql = 'SELECT email, password FROM profiles';
     db.query(sql, (error, result) => {
         if(error) {
             throw error;

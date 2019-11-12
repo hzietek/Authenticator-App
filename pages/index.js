ea4640edaf95aaa3fcad93d765ca1179cd18d512
unknown <hubazietek3@gmail.com>
2019-11-12 23:35:34 +0100

    style changes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import '../style/login.scss';
 
 const Index = () => (
   <div className='container'>
-    <div className="title"><h1>AUTHENTICATOR</h1></div>
+    <div className="title"><h1>AUTHENTICATOR</h1><div className="auth-image"></div></div>
     <div className='login-container'>
       <Login></Login>
       <Register></Register>
